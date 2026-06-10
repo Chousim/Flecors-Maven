f630636a7c4b9c358e9f4d5d7a836cb3914404fa
Chousim <carlosyisjv@gmail.com>
2026-06-09 23:17:35 -0600

    V1

---
+++ backend/src/main/java/com/flecors/App.java
@@ -0,0 +1,13 @@
+package com.flecors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
